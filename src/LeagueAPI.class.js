a463b738f46a1b88bf3d730ca0ba5a58fcabdb39
Josh Fillingham (Fill) <jjfillingham@gmail.com>
2022-06-21 21:17:28 +0100

    fix: only take gametime from LeagueAPI

--- src/LeagueAPI.class.js
+++ src/LeagueAPI.class.js
@@ -43,12 +43,8 @@ function httpRequest() {
 }
 
 export class LeagueAPI extends EventEmitter {
-    constructor(starting_left_towers = 0, starting_right_towers = 0, starting_left_kills = 0, starting_right_kills = 0) {
+    constructor() {
         super()
-        this.starting_left_towers = starting_left_towers
-        this.starting_right_towers = starting_right_towers
-        this.starting_left_kills = starting_left_kills
-        this.starting_right_kills = starting_right_kills
 
         this.disconnected = false
 
@@ -65,35 +61,7 @@ export class LeagueAPI extends EventEmitter {
             const time_seconds = Math.floor(data.gameData.gameTime - (time_minutes * 60)).toString().padStart(2, 0)
             const gametime = `${time_minutes}:${time_seconds}`
 
-            let left_towers = this.starting_left_towers
-            let right_towers = this.starting_right_towers
-            let left_kills = this.starting_left_kills
-            let right_kills = this.starting_right_kills
-
-            data.events.Events.filter(event => event.EventTime <= data.gameData.gameTime).forEach(event => {
-                switch (event.EventName) {
-                    case 'TurretKilled': {
-                        if (event.TurretKilled.startsWith("Turret_T1")) {
-                            right_towers++
-                        } else if (event.TurretKilled.startsWith("Turret_T2")) {
-                            left_towers++
-                        }
-                        break
-                    }
-
-                    case 'ChampionKill': {
-                        const foundPlayer = data.allPlayers.find(player => player.summonerName == event.KillerName)
-                        if (foundPlayer) {
-                            foundPlayer.team == "CHAOS" ? right_kills++ : left_kills++
-                        }
-                        break
-                    }
-                }
-
-
-            })
-
-            this.emit("update", { gametime, left_towers, right_towers, left_kills, right_kills })
+            this.emit("update", { gametime })
         } catch (error) {
             if (error.code === 'ECONNREFUSED') {
                 if (!this.disconnected) {
